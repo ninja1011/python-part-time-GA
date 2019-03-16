cf93de8767f561f4dd5470802d77eba2fa8cedac
ninja1011 <ninja1011@gmail.com>
2019-03-16 14:19:13 -0400

    Added Comments file

---
+++ comments.py
@@ -0,0 +1,16 @@
+print("check time")
+print("if time is 9:30, clock out of work")
+# Add an else statement for when the time isn't ok
+print("walk to nearest train station")
+# how to get to the train station
+print("check to see if your metro card fare is above $6.75")
+print("If fare is sufficient, leave train station. If not, add funds to the metro card")
+# Add procedure on how to add funds to metrocard
+print("ok proceed to bus stop")
+print("not pay fare")
+print("proceed to bus stop")
+print("check bus time")
+print("wait for bus")
+print("board bus")
+print("pay fare")
+
